acecbf15861d7ba73ce91902a70495df07ca5b5d
Michael Hucka <mhucka@caltech.edu>
2017-01-25 15:39:59 -0800

    Massive refactoring + use colorama for pretty output.

--- harvesting-toolkit/tools/url-check/check-ia.py
+++ harvesting-toolkit/tools/url-check/check-ia.py
@@ -21,11 +21,12 @@ Wayback Machine API description: https://archive.org/help/wayback_api.php
 
 '''
 
-__version__ = '0.11'
+__version__ = '1.0'
 __author__  = 'Michael Hucka <mhucka@caltech.edu>'
 __email__   = 'mhucka@caltech.edu'
 __license__ = 'Public domain'
 
+import colorama
 import http
 import json
 import os
@@ -49,6 +50,8 @@ def main(file=None, okay=None, miss=None, quiet=False, *args):
         raise ValueError('No input file and no URLs given -- nothing to do')
     elif file and not os.path.exists(file):
         raise ValueError('File "{}" not found'.format(file))
+    if not quiet:
+        colorama.init(autoreset=True)
     # Let's do this thing.
     run(file, args, okay, miss, quiet)
 
@@ -68,113 +71,131 @@ main.__annotations__ = dict(
 # Main functions
 # ......................................................................
 
-def run(input_file, urls, found_file, notfound_file, quiet=False):
-    successes = []
-    failures  = []
+def run(input_filename, urls, found_filename, notfound_filename, quiet=False):
+    found_file     = None
+    found_count    = 0
+    notfound_file  = None
+    notfound_count = 0
 
-    if input_file:
-        with open(input_file, encoding="utf-8") as f:
-            for line in f:
+    if found_filename:
+        found_file = open(found_filename, 'w')
+    if notfound_filename:
+        notfound_file = open(notfound_filename, 'w')
+
+    if input_filename:
+        with open(input_filename, encoding="utf-8") as f:
+            for idx, line in enumerate(f):
                 url = line.strip()
                 if '/' not in url:             # Skip non-URLs
                     continue
-                (found, result) = check(url, quiet)
-                if found:
-                    successes.append(result)
+                if check(url, idx, found_file, notfound_file, quiet):
+                    found_count += 1
                 else:
-                    failures.append(url)
+                    notfound_count += 1
     else:
-        for url in urls:
-            (found, result) = check(url, quiet)
-            if found:
-                successes.append(result)
+        for idx, url in enumerate(urls):
+            if check(url, idx, found_file, notfound_file, quiet):
+                found_count += 1
             else:
-                failures.append(url)
+                notfound_count += 1
 
-    if not quiet and (successes or failures):
-        msg('-'*70)
-    if successes:
-        if found_file:
-            with open(found_file, 'w') as out:
-                count = 0
-                for entry in successes:
-                    out.write(entry[0])
-                    out.write(',')
-                    out.write(entry[1])
-                    out.write(',')
-                    out.write(entry[2])
-                    out.write('\n')
-                    count += 1
-            if not quiet:
-                msg('{} successes written to file "{}"'.format(count, found_file))
-        else:
-            msg('{} URLs found in IA'.format(len(successes)))
-    elif not quiet:
-        if found_file:
-            msg('No successes -- {} not written'.format(found_file))
-        else:
-            msg('No successes')
-    if failures:
-        if notfound_file:
-            with open(notfound_file, 'w') as out:
-                count = 0
-                for url in failures:
-                    out.write(url)
-                    out.write('\n')
-                    count += 1
-            if not quiet:
-                msg('{} failures written to file "{}"'.format(count, notfound_file))
-        else:
-            msg('{} URLs not found in IA'.format(len(failures)))
-    elif not quiet:
-        if notfound_file:
-            msg('No failures -- {} not written'.format(notfound_file))
-        else:
-            msg('No failures')
+    if not quiet and (found_count > 0 or notfound_count > 0):
+        print('')
+    if found_filename:
+        found_file.close()
+        if not quiet:
+            if found_count:
+                info('{} URLs found, written to file "{}"'.format(found_count, found_filename))
+            else:
+                info('No URLs found.')
+    elif found_count > 0 and not quiet:
+        info('{} URLs found'.format(found_count))
+    if notfound_filename:
+        notfound_file.close()
+        if not quiet:
+            if notfound_count:
+                info('{} URLS not found, written to file "{}"'.format(notfound_count, notfound_filename))
+            else:
+                info('All URLs found')
+    elif notfound_count > 0 and not quiet:
+        info('{} URLs not found'.format(notfound_count))
+    if not quiet:
+        info('Done.')
+        print(colorama.Style.RESET_ALL)
 
 
 # Utilities
 # ......................................................................
 
-def check(given_url, quiet=False):
-    final_url = get_final_url(given_url)
-    if not final_url:
-        if not quiet:
-            msg('Could not access {} -- skipping'.format(given_url))
-        return (False, given_url)
-    url = 'http://archive.org/wayback/available?url=' + final_url
+def check(given_url, idx, found_file, notfound_file, quiet=False):
     try:
-        response = urlopen(url)
-        data = json.loads(response.read().decode('utf-8'))
-        if data['archived_snapshots']:
-            archive_info = data['archived_snapshots']
-            if 'closest' not in archive_info:
-                if not quiet:
-                    msg('Got unexpected result for {}'.format(given_url))
-                return (False, given_url)
-            ts = archive_info['closest']['timestamp']
-            status = archive_info['closest']['status']
-            ia_url = archive_info['closest']['url']
-            if not quiet:
-                if int(status) > 300:
-                    msg('*** abnormal status {} for {}'.format(status, given_url))
-                else:
-                    msg('Found: {}'.format(ts, given_url))
-            return (True, (ts, given_url, ia_url))
+        (found, ts, ia_url, status) = url_in_ia(given_url)
+        if not found:
+            # Try to dereference the original URL, in case the origin
+            # server redirects to another URL and that's what IA has.
+            final_url = get_final_url(given_url)
+            (found, ts, ia_url, status) = url_in_ia(final_url)
+        if not found:
+            warning('[{}] {} -- not found in IA'.format(idx, given_url))
+            log(notfound_file, given_url)
+            return False
+        elif not ts:
+            warning('[{}] {} -- found, but no data available'.format(idx, given_url))
+            log(found_file, given_url, '00000000000000')
+            return True
+        elif int(status) > 300:
+            warning('[{}] {} -- found, but IA record has status {}'.format(idx, given_url, status))
+            log(notfound_file, given_url)
+            return False
         else:
-            if not quiet:
-                msg('Not found: {}'.format(given_url))
-            return (False, given_url)
+            good('[{}] {} -- archived {}'.format(idx, given_url, ts))
+            log(found_file, given_url, ts)
+            return True
     except Exception as e:
-        msg('Unable to check {}'.format(given_url))
-        msg(e)
-        return (False, given_url)
+        bad('Unable to check {}'.format(given_url))
+        bad(str(e))
+        return False
+
+
+def url_in_ia(url):
+    '''Returns a tuple: (found, timestamp, archived_url, status_code)
+    where "found" is a Boolean indicating whether the URL was found in IA,
+    and the remaining tuple values are the data found in IA if the URL was
+    indeed found.'''
+
+    test_url = 'http://archive.org/wayback/available?url=' + url
+    response = urlopen(test_url)
+    data = json.loads(response.read().decode('utf-8'))
+    if 'archived_snapshots' in data:
+        archive_info = data['archived_snapshots']
+        if 'closest' not in archive_info:
+            # This can happen when IA reports positively for the URL but
+            # provides no additional data.  According to the API docs, this
+            # happens "If the url is not available (not archived or currently
+            # not accessible)"; however, I have seen it happen when an
+            # archived URL is a redirection to another page within IA itself.
+            # Example case: https://www3.epa.gov/node/156853
+            # To avoid false positives, we treat this as a failure, but we
+            # need to tell IA about this ambiguity.
+            return (False, None, None, None)
+        ts = archive_info['closest']['timestamp']
+        ia_url = archive_info['closest']['url']
+        status = archive_info['closest']['status']
+        return (True, ts, ia_url, status)
+    else:
+        return (False, None, None, None)
 
 
 def get_final_url(url):
+    '''Attempt to read the given "url" using HTTP HEAD and return the
+    final URL after any redirections.'''
+
+    def get_head(url):
+        return requests.head(url, allow_redirects=True, verify=False, timeout=10)
+
     try:
         requests.packages.urllib3.disable_warnings()
-        resp = requests.get(url, verify=False, timeout=10)
+        resp = get_head(url)
         if resp.history:
             return resp.history[-1].url
         elif url[-1] != '/':
@@ -182,21 +203,42 @@ def get_final_url(url):
             return get_final_url(url)
         else:
             # See if the host has a number in the name, like "www3.epa.gov".
-            # Remove the number and try again.
             u = urlparse(url)
             if re.search('\d', u.netloc):
+                # Remove the number and try again.
                 loc = re.sub('[0-9]', '', u.netloc)
                 url = u.scheme + '://' + loc + u.path
-                return get_final_url(url)
+                # This time, we don't want to go through the rediretion step
+                # because it'll probably send us back to the original.
+                resp = get_head(url)
+                if resp.history:
+                    return url
         # If we get here, all our attempts failed.  Leave the URL as it was
         # and hope for the best.
         return url
     except Exception as e:
-        msg('Encountered problem dereferencing {}'.format(url))
-        msg(e)
+        bad('Encountered problem dereferencing {}'.format(url))
+        bad(str(e))
 
 
-def msg(text):
+def log(dest_file, *args):
+    if dest_file:
+        dest_file.write(','.join(args) + '\n')
+
+
+def bad(text):
+    print(colorama.Fore.RED + text, flush=True)
+
+
+def good(text):
+    print(colorama.Fore.GREEN + text, flush=True)
+
+
+def warning(text):
+    print(colorama.Fore.YELLOW + text, flush=True)
+
+
+def info(text):
     print(text, flush=True)
 
 
